8788ff260ab1288a03eb2aa63b1bc9d2f8c14b48
s30893 <s30893@pjwstk.edu.pl>
2025-05-18 20:19:02 +0200

    Dialogue function added
    A dialog feature and the use of special keys have been added. Files have been organized.

--- newJSFile.js
+++ newJSFile.js
@@ -1,5 +1,5 @@
 function openGame(){
-    window.location.href = "gamePage.html";
+    window.location.href = "gamePageNew.html";
 }
 const mapData = [
     ["f", " ", " ", " ", " ", " ", " ", " "],
@@ -13,6 +13,7 @@ const tileSize = 90;
 const mapWidth = 8;
 const mapHeight = 5;
 let playerPosition = {x: 1, y: 1};
+let hint;
 
 function generateMap() {
     const map = document.querySelector(".map");
@@ -45,6 +46,12 @@ function generateMap() {
     const player = document.createElement("div");
     player.classList.add("player");
     map.appendChild(player);
+
+    hint = document.createElement("div");
+    hint.classList.add("hint");
+    hint.classList.add("hidden");
+    hint.textContent = "Naciśnij E";
+    document.querySelector(".map").appendChild(hint);
 }
 
 function updatePlayerPosition() {
@@ -82,6 +89,34 @@ document.addEventListener("keydown", (e) => {
         }
     }
     updatePlayerPosition();
+    //dialog with home
+    let x = playerPosition.x;
+    let y = playerPosition.y;
+    if (
+        mapData[y - 1]?.[x] === "h" ||
+        mapData[y + 1]?.[x] === "h" ||
+        mapData[y]?.[x - 1] === "h" ||
+        mapData[y]?.[x + 1] === "h"
+    ) {
+        hint.classList.remove("hidden");
+        hint.style.left = `${x * tileSize + 10}px`;
+        hint.style.top = `${y * tileSize}px`;
+    } else {
+        hint.classList.add("hidden");
+    }
+    if(e.key === "e" || e.key === "E"){
+        if(mapData[y-1]?.[x] === "h" || mapData[y+1]?.[x] === "h" || mapData[y]?.[x-1] === "h" || mapData[y]?.[x+1] === "h"){
+            let dialog = document.querySelector(".dialog");
+            dialog.classList.remove("hidden");
+            document.addEventListener("keydown", (e) => {
+                if (e.key === "Enter") {
+                    const dialog = document.querySelector(".dialog");
+                    dialog.classList.add("hidden");
+                }
+            });
+
+        }
+    }
 });
 
 window.onload = function () {
